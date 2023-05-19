0dfb0380ecb2a6403eb3e89ab609314e5fccdf69
Festus <matkowsky808@gmail.com>
2023-05-19 21:08:01 +0200

    fix: clean code in testimonials

--- app/components/modules/Testimonials/index.tsx
+++ app/components/modules/Testimonials/index.tsx
@@ -13,24 +13,12 @@ const Testimonials = () => {
 	const responsiveSlidesPerView = () => {
 		let value = 1.3
 
-		if (isMD) {
-			value = 1.5
-		}
-		if (isLG) {
-			value = 2
-		}
-		if (isXL) {
-			value = 2.25
-		}
-		if (is2XL) {
-			value = 2.5
-		}
-		if (is3XL) {
-			value = 3
-		}
-		if (is4XL) {
-			value = 4
-		}
+		if (isMD) value = 1.5
+		if (isLG) value = 2
+		if (isXL) value = 2.25
+		if (is2XL) value = 2.5
+		if (is3XL) value = 3
+		if (is4XL) value = 4
 
 		return value
 	}
