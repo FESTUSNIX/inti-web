6d84daa81473c8db7d4ab0e717e357d301db2b71
Festus <matkowsky808@gmail.com>
2023-05-26 13:46:46 +0200

    refractor: revive old layout

--- app/components/modules/OurAdvantages/components/modules/Card/index.tsx
+++ app/components/modules/OurAdvantages/components/modules/Card/index.tsx
@@ -4,15 +4,9 @@ type Props = {
 	index: number
 }
 
-const bgs = ['red', 'blue', 'yellowgreen', 'gray']
-
 const Card = ({ index }: Props) => {
-	
-
 	return (
-		<article
-			className='relative grow overflow-hidden rounded-lg border border-dark-700/20 p-8 opacity-50'
-			style={{ backgroundColor: bgs[index] }}>
+		<article className='relative grow overflow-hidden rounded-lg border border-dark-700/20 p-8'>
 			<h3 className='mb-4 text-xl font-bold text-neutral-300 lg:text-2xl'>Lorem ipsum dolor</h3>
 			<p className='text-neutral-400'>
 				Lorem ipsum dolor sit, amet consectetur adipisicing elit. Harum placeat aut, accusantium quaerat nesciunt veniam
--- app/components/modules/OurAdvantages/index.tsx
+++ app/components/modules/OurAdvantages/index.tsx
@@ -1,122 +1,23 @@
 'use client'
 
-import React, { useRef, useEffect, useState } from 'react'
+import React from 'react'
 import SectionHeading from '../../elements/SectionHeading'
 import Card from './components/modules/Card'
-import { motion, useDragControls, animate, useMotionValue, useTransform } from 'framer-motion'
-import Button from '../../elements/Button'
 
 const OurAdvantages = () => {
-	const constraints = useRef<HTMLDivElement>(null)
-
-	const [constraintsHeight, setConstraintsHeight] = useState(0)
-	const itemEls = useRef<HTMLDivElement[]>(new Array())
-
-	const [isReset, setIsReset] = useState(false)
-
-	// const y = useTransform(isReset, [false, true], [0, 100])
-
-	useEffect(() => {
-		const currentHeight = (constraints.current?.children[0].children[0] as HTMLDivElement)?.offsetHeight
-
-		if (currentHeight !== constraintsHeight) setConstraintsHeight(currentHeight)
-	}, [constraints.current])
-
-	function getTranslateXY(element: HTMLDivElement) {
-		const style = window.getComputedStyle(element)
-		const matrix = new DOMMatrixReadOnly(style.transform)
-		return {
-			translateX: matrix.m41,
-			translateY: matrix.m42
-		}
-	}
-
-	useEffect(() => {
-		console.log('IS_RESET', isReset)
-	}, [isReset])
-
-	const resetPosition = () => {
-		setIsReset(false)
-		setTimeout(() => {
-			setIsReset(true)
-		}, 1)
-	}
-
 	return (
-		<section className='relative mb-32 border-y border-dark-700/20 bg-dark-800/20'>
-			<div className='wrapper flex flex-col lg:flex-row lg:gap-32 '>
-				<div>
-					<SectionHeading className='mb-12 py-12 max-lg:text-center'>
-						Wszystko czego potrzebuje twoja firma
-					</SectionHeading>
-					<Button onClick={() => resetPosition()} className='text-2xl'>
-						TOGGLE
-					</Button>
-				</div>
+		<section className='relative mb-32 border-y border-dark-700/20 bg-dark-800/20 py-12'>
+			<div className='wrapper flex flex-col lg:gap-16'>
+				<SectionHeading className='mb-8 self-center max-lg:text-center'>
+					Wszystko czego potrzebuje twoja firma
+				</SectionHeading>
 
-				<div
-					className='flex basis-1/2 gap-12 overflow-hidden'
-					ref={constraints}
-					style={{ maxHeight: constraintsHeight * 0.8 }}>
-					{Array(1)
+				<div className='grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 xl:gap-8'>
+					{Array(6)
 						.fill(0)
 						.map((_, index) => (
-							<motion.div
-								key={index}
-								ref={element => itemEls.current.push(element!)}
-								drag='y'
-								style={{
-									// y: -constraintsHeight,
-									...(isReset
-										? {
-												y: -constraintsHeight,
-												x: 0
-										  }
-										: {})
-								}}
-								onDrag={(event, info) => {
-									// console.log(info.offset.x, info.offset.y, info.point.y, event.layerY)
-									// console.log(itemEls.current[index].offsetHeight)
-									// console.log(getTranslateXY(itemEls.current[index]).translateY, constraintsHeight)
-									if (
-										getTranslateXY(itemEls.current[index]).translateY >= 0 ||
-										getTranslateXY(itemEls.current[index]).translateY <= -(constraintsHeight * 2)
-									) {
-										console.log('yyeeeeeah')
-										// setIsReset(true)
-										resetPosition()
-									} else {
-										// console.log('noooo', isReset)
-										// setIsReset(false)
-									}
-								}}
-								className='cursor-grab  active:cursor-grabbing'>
-								{Array(3)
-									.fill(0)
-									.map((_, index) => (
-										<div key={index} className='flex flex-col gap-12 pb-12'>
-											{Array(3)
-												.fill(0)
-												.map((_, index) => (
-													<Card key={index} index={index} />
-												))}
-										</div>
-									))}
-							</motion.div>
+							<Card key={index} index={index} />
 						))}
-					{/* <motion.div drag='y' style={{ y: -constraintsHeight }} className='cursor-grab active:cursor-grabbing'>
-						{Array(3)
-							.fill(0)
-							.map((_, index) => (
-								<div key={index} className='flex flex-col gap-12 pb-12'>
-									{Array(3)
-										.fill(0)
-										.map((_, index) => (
-											<Card key={index} index={index} />
-										))}
-								</div>
-							))}
-					</motion.div> */}
 				</div>
 
 				<div className='gradient-blob-1 absolute left-1/2 top-0 -z-10 -translate-x-1/2'></div>
