26766dd4f0b071771e9b3915d9fd78cec7f67763
Festus <matkowsky808@gmail.com>
2023-05-27 21:47:07 +0200

    refractor: change to more efficient useScrollPosition hook

--- app/(main)/components/modules/Navbar/components/elements/Background/index.tsx
+++ app/(main)/components/modules/Navbar/components/elements/Background/index.tsx
@@ -1,15 +1,26 @@
 'use client'
 
-import React from 'react'
-import { useScrollPosition } from '@/app/(main)/hooks/useScrollPosition'
+import { useScrollPosition } from '@n8tb1t/use-scroll-position'
+import clsx from 'clsx'
+import React, { useState } from 'react'
 
 export const Background = () => {
-	const scrollPosition = useScrollPosition()
+	const [isActive, setIsActive] = useState(false)
+
+	useScrollPosition(
+		({ currPos }) => {
+			const isShow = currPos.y < 10
+
+			if (isShow !== isActive) setIsActive(isShow)
+		},
+		[isActive]
+	)
 
 	return (
 		<div
-			className={`${
-				scrollPosition > 10 ? 'border-dark-700/20 bg-dark-800/20 backdrop-blur-md' : 'border-transparent'
-			}  absolute inset-0 -z-10 border-b transition-colors duration-700`}></div>
+			className={clsx(
+				'absolute inset-0 -z-10 border-b transition-colors duration-700',
+				isActive ? 'border-dark-700/20 bg-dark-800/20 backdrop-blur-md' : 'border-transparent'
+			)}></div>
 	)
 }
