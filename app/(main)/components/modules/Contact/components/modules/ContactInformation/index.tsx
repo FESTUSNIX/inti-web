564c9d7719d3b9fe70de1f844e1c6608e2c724f6
Festus <matkowsky808@gmail.com>
2023-05-29 10:14:12 +0200

    chore: Add aria-labels to social media links

--- app/(main)/components/modules/Contact/components/modules/ContactInformation/index.tsx
+++ app/(main)/components/modules/Contact/components/modules/ContactInformation/index.tsx
@@ -24,21 +24,25 @@ const ContactInformation = () => {
 
 			<div className='mt-8 flex max-sm:justify-between sm:gap-8 lg:mt-auto'>
 				<Link
+				aria-label='Our Instagram'
 					href={''}
 					className='rounded-full border border-transparent p-3 text-neutral-400 duration-300 hover:border-dark-700/20 hover:bg-dark-800/20'>
 					<FaInstagram size={24} />
 				</Link>
 				<Link
+				aria-label='Our Facebook'
 					href={''}
 					className='rounded-full border border-transparent p-3 text-neutral-400 duration-300 hover:border-dark-700/20 hover:bg-dark-800/20'>
 					<FaFacebook size={24} />
 				</Link>
 				<Link
+				aria-label='Our Twitter'
 					href={''}
 					className='rounded-full border border-transparent p-3 text-neutral-400 duration-300 hover:border-dark-700/20 hover:bg-dark-800/20'>
 					<FaTwitter size={24} />
 				</Link>
 				<Link
+				aria-label='Our TikTok'
 					href={''}
 					className='rounded-full border border-transparent p-3 text-neutral-400 duration-300 hover:border-dark-700/20 hover:bg-dark-800/20'>
 					<FaTiktok size={24} />
