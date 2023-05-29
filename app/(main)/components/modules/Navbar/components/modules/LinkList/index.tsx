e7c4b4617aa4bf5192e6fd908c6e33db91b45f36
Festus <matkowsky808@gmail.com>
2023-05-29 22:04:23 +0200

    style: Change link font weight to regular

--- app/(main)/components/modules/Navbar/components/modules/LinkList/index.tsx
+++ app/(main)/components/modules/Navbar/components/modules/LinkList/index.tsx
@@ -9,7 +9,7 @@ const LinkList = () => {
 				<li key={link} className='group relative cursor-pointer'>
 					<Link
 						href={link}
-						className='group-last:gradient-wave relative flex w-max items-center rounded-lg px-4 py-2 text-lg font-black uppercase text-neutral-200 group-last:bg-gradient-brand'>
+						className='group-last:gradient-wave relative flex w-max items-center rounded-lg px-4 py-2 text-lg uppercase text-neutral-200 group-last:bg-gradient-brand'>
 						{displayName}
 					</Link>
 					<div className='absolute left-0 top-1/2 h-1 w-full origin-right -translate-y-1/2 scale-x-0 bg-gradient-brand duration-300 group-last:hidden group-hover:scale-x-100'></div>
