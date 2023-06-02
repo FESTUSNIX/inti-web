cf417e2419af6b0bd3fd591f9b139aa4fb06cd9c
Festus <matkowsky808@gmail.com>
2023-06-02 23:36:59 +0200

    style: Add background blobs

--- app/(main)/components/modules/OurAdvantages/index.tsx
+++ app/(main)/components/modules/OurAdvantages/index.tsx
@@ -8,14 +8,15 @@ const OurAdvantages = () => {
 	return (
 		<section className='relative mb-32 border-y border-dark-700/20 bg-dark-800/20 py-12'>
 			<div className='wrapper flex flex-col lg:gap-16'>
-				<SectionHeading className='mb-8 self-center max-lg:text-center'>
-					Wszystko czego potrzebuje twoja firma
-				</SectionHeading>
+				<div className='mb-8 self-center text-center'>
+					<SectionHeading label='nasze zalety'>Wszystko czego potrzebuje twoja firma</SectionHeading>
+				</div>
 
 				{/* @ts-expect-error Server Component */}
 				<Cards />
 
-				<div className='gradient-blob-1 absolute left-1/2 top-0 -z-10 -translate-x-1/2'></div>
+				<div className='blob-our-advantages-1 blob left-2/4 top-0 -translate-x-1/2' />
+				<div className='blob blob-our-advantages-2 absolute bottom-0 right-0 h-1/2 w-1/4 -translate-x-1/2 opacity-30' />
 			</div>
 		</section>
 	)
--- app/(main)/components/modules/WebsitePros/components/modules/Cards/index.tsx
+++ app/(main)/components/modules/WebsitePros/components/modules/Cards/index.tsx
@@ -6,7 +6,8 @@ const Cards = async () => {
 	const cards = await getWebsiteProsCards()
 
 	return (
-		<div className='flex shrink-0 flex-col gap-8 md:gap-16 lg:basis-1/2'>
+		<div className='relative flex shrink-0 flex-col gap-8 md:gap-16 lg:basis-1/2'>
+			<div className='blob blob-website-pros left-0 top-1/2 h-full w-full -translate-x-1/2 -translate-y-1/2'></div>
 			{cards.map((card, index) => (
 				<Card key={card._id} index={index} title={card.title} content={card.content} />
 			))}
