6f32049ef2d04b40861b7a0a1658da95b1586ba8
Festus <matkowsky808@gmail.com>
2023-06-03 17:30:38 +0200

    feat: Add hero image

--- app/(main)/components/modules/Hero/index.tsx
+++ app/(main)/components/modules/Hero/index.tsx
@@ -5,8 +5,8 @@ import Image from 'next/image'
 
 const Hero = () => {
 	return (
-		<header className='wrapper mb-4 mt-24 flex min-h-[calc(100vh-84px)] flex-col items-center pb-16 md:flex-row md:gap-16 md:pb-24'>
-			<div className='max-w-full basis-3/5'>
+		<header className='wrapper mb-4 mt-24 flex flex-col items-center gap-16 pb-16 max-lg:pt-24 md:min-h-[calc(100vh-84px)] md:flex-row md:pb-24'>
+			<div className='max-w-full basis-3/4 2xl:basis-3/5'>
 				<h1 className='mb-4 max-w-full font-heading text-4xl font-black capitalize text-neutral-50 sm:text-5xl lg:text-6xl lg:leading-tight'>
 					<span>Wyprzedź&nbsp;</span>
 					<span className='inline-block'>konkurencje i&nbsp;</span>
@@ -16,7 +16,7 @@ const Hero = () => {
 					<span className='bg-gradient-brand bg-clip-text text-transparent'>rozpoznawalny!</span>
 				</h1>
 
-				<h2 className='mb-8 max-w-xl text-lg text-neutral-400 lg:text-2xl'>
+				<h2 className='mb-8 max-w-xl text-lg text-neutral-400 lg:text-xl'>
 					Docieraj do większej ilości klientów dzięki dedykowanej stronie internetowej.
 				</h2>
 
@@ -30,9 +30,13 @@ const Hero = () => {
 				</div>
 			</div>
 
-			<div className='relative mt-4 flex w-full basis-2/5'>
-				<Image src={heroImg} alt='us coding' className='relative left-0 top-1/2 scale-125' />
-				<div className='gradient-blob-2 absolute min-h-full min-w-full scale-[1.5]'></div>
+			<div className='relative -z-10 mt-4 flex w-full basis-1/4 2xl:basis-2/5'>
+				<Image
+					src={heroImg}
+					alt='us coding'
+					className='relative top-1/2 scale-125 md:absolute md:left-0 md:min-w-[340px] md:-translate-y-1/2 md:scale-150 2xl:scale-125'
+				/>
+				<div className='blob blob-hero-image aspect-square min-h-full min-w-full scale-150 md:-translate-y-1/2 md:scale-[5] 2xl:scale-150'></div>
 			</div>
 		</header>
 	)
