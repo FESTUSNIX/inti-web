3f36d52898ff87fb53dfe929a256113d8ebe4d35
Festus <matkowsky808@gmail.com>
2023-06-09 16:13:27 +0200

    chore: Change Header to Hero

--- app/(main)/services/components/modules/Hero/index.tsx
+++ app/(main)/services/components/modules/Hero/index.tsx
@@ -6,13 +6,13 @@ import lightbulb from '../../../assets/lightbulb-hand-1.png'
 import Parallax from '@/app/(main)/components/elements/Parallax'
 import MouseParallax from '@/app/(main)/components/elements/MouseParallax'
 
-const Header = () => {
+const Hero = () => {
 	return (
 		<header className='relative flex min-h-screen flex-col items-center' style={{ clipPath: 'inset(0)' }}>
 			<div className='wrapper'>
 				<div className='flex flex-col items-center gap-4 pt-36 text-center md:pt-48'>
 					<h1 className='text-gradient relative z-20 max-w-md px-4 font-heading text-xl font-bold uppercase md:px-8 md:text-2xl xl:text-3xl'>
-						strony internetowe XXI wieku
+						lorem ipsum dolor
 					</h1>
 					<div className='relative text-7xl font-black uppercase sm:text-8xl md:text-9xl xl:text-[168px]'>
 						<div className='pointer-events-none -z-10 text-neutral-50' aria-hidden='true'>
@@ -43,4 +43,4 @@ const Header = () => {
 	)
 }
 
-export default Header
+export default Hero
--- app/(main)/services/page.tsx
+++ app/(main)/services/page.tsx
@@ -1,15 +1,15 @@
 import React from 'react'
-import Header from './components/modules/Header'
 import ContactCTA from './components/modules/ContactCTA'
 import ServicesCards from './components/modules/ServicesCards'
 import ServiceDescriptions from './components/modules/ServiceDescriptions'
 import OurCapabilities from './components/modules/OurCapabilities'
 import Process from './components/modules/Process'
+import Hero from './components/modules/Hero'
 
 const ServicesPage = () => {
 	return (
 		<>
-			<Header />
+			<Hero />
 			<ContactCTA />
 			<OurCapabilities />
 			<ServicesCards />
